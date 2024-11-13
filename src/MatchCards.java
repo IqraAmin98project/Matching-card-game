ed5e69a94459164a5b0a233461a08ce2e064af6f
Iqra <iqraamin096@gmail.com>
2024-11-13 23:31:27 +0500

    my second commit

--- src/MatchCards.java
+++ src/MatchCards.java
@@ -1,2 +1,50 @@
+import javax.swing.*;
+import java.io.*;
+
 public class MatchCards {
+    class card {
+        private String cardName;
+        private ImageIcon cardImageIcon;
+
+        public card(String cardName, ImageIcon cardImageIcon) {
+            this.cardName = cardName;
+            this.cardImageIcon = cardImageIcon;
+        }
+
+        public String getCardName() {
+            return cardName;
+        }
+
+        public void setCardName(String cardName) {
+            this.cardName = cardName;
+        }
+
+        public ImageIcon getCardImageIcon() {
+            return cardImageIcon;
+        }
+
+        public void setCardImageIcon(ImageIcon cardImageIcon) {
+            this.cardImageIcon = cardImageIcon;
+        }
+
+
+        public String toString() {
+            return cardName;
+        }
+    }
+
+    //Game configuration
+    String[] cardlist = {
+            "darkness",
+            "double",
+            "fairy",
+            "fighting",
+            "fire",
+            "grass",
+            "lighting",
+            "metal",
+            "psychic",
+            "water"
+    };
+
 }
