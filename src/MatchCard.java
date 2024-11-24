ed567c3ed700f69972921f1a22556e362329df32
IqraAmin98project/Matching-card-game <tasbeehaabrar@gmail.com>
2024-11-24 00:02:34 -0800

    First commit

--- src/MatchCard.java
+++
@@ -1,9 +0,0 @@
-class Scoreboard {
-    private ArrayList<ScoreEntry> scores;
-    private final String scoreFilePath = "scoreboard.txt"; // File to store scores
-
-    public Scoreboard() {
-        scores = new ArrayList<>();
-        loadScores(); // Load scores from file
-    }
-}
