2b0bb40baa9cabd09945852112990839290a542f
IqraAmin98project/Matching-card-game <tasbeehaabrar@gmail.com>
2024-11-23 23:54:48 -0800

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
-    public ScoreEntry(String playerName, double time, int errors, boolean isSinglePlayer) {
-        this.playerName = playerName;
-        this.time = time;
-        this.errors = errors;
-        this.isSinglePlayer = isSinglePlayer;
-    }
+class Scoreboard {
+    private ArrayList<ScoreEntry> scores;
+    private final String scoreFilePath = "scoreboard.txt"; // File to store scores
 
-    // Getter methods
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public double getTime() {
-        return time;
-    }
-
-    public int getErrors() {
-        return errors;
-    }
-
-    public boolean isSinglePlayer() {
-        return isSinglePlayer;
+    public Scoreboard() {
+        scores = new ArrayList<>();
+        loadScores(); // Load scores from file
     }
 }
