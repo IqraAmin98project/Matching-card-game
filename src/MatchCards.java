78ac6b7dbfb1fe18fc321e99e0d1e88016957bae
IqraAmin98project/Matching-card-game <tasbeehaabrar@gmail.com>
2024-11-24 09:13:03 -0800

    Second commit

--- src/MatchCards.java
+++ src/MatchCards.java
@@ -173,6 +173,24 @@ private void loadScores() {
         }
     }
 }
+public class MatchCards {
+    private Scoreboard scoreboard;
+    double currentPlayerTime = 0.0; // Initialize time
+
+    class Card {
+        String cardName;
+        ImageIcon cardImageIcon;
+
+        Card(String cardName, ImageIcon cardImageIcon) {
+            this.cardName = cardName;
+            this.cardImageIcon = cardImageIcon;
+        }
+
+        public String toString() {
+            return cardName;
+        }
+    }
+
 
 
 
